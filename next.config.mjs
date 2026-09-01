387daf83fcd20cafe057ce7d44d04bfb15be3d28
sonugg07 <mohit9815438331@gmail.com>
2026-09-01 18:58:11 +0530

    Configure no-cache headers in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,27 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'pbs.twimg.com', 'avatars.githubusercontent.com'],
   },
+  generateEtags: false,
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=0, must-revalidate',
+        },
+      ],
+    },
+    {
+      source: '/api/:path*',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-store, no-cache, must-revalidate, max-age=0',
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
